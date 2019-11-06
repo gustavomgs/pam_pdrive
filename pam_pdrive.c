ab0fb01fdf3bedcf2cb6869bc5c2911d3edcf3d9
gustavomgs <gustavomgs13@gmail.com>
2019-11-06 08:58:27 -0300

    Version 1.3

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -23,6 +23,7 @@
 #include <pwd.h>
 #include <security/pam_modules.h>
 #include <security/pam_ext.h>
+#include <zconf.h>
 
 #define _GNU_SOURCE 
     
@@ -45,6 +46,8 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
     char buf[256], *p;
     char buf2[256];
     int i;
+    int user_root;
+    user_root = getuid();
    
     len = readlink("/sys/block/sdb", buf, 256);
    
@@ -69,55 +72,74 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
  
     buf[len - 1 ] = 0;
 
+    if(user_root == 0){
+	system("clear");
+	
+	FILE *log;
+	log = fopen("/etc/pam.d/pam.pdrive/log/login", "a");
+	fprintf(log, "%s", "-> Usuário Root | ");
+	fprintf(log, "%d/", data_hora_atual->tm_mday);
+	fprintf(log, "%d/", data_hora_atual->tm_mon+1);
+	fprintf(log, "%d ", data_hora_atual->tm_year+1900);
+	fprintf(log, "%d:", data_hora_atual->tm_hour);
+	fprintf(log, "%d:", data_hora_atual->tm_min);
+	fprintf(log, "%d\n ", data_hora_atual->tm_sec);
 
-      FILE *g= fopen("/etc/pam.d/pam.pdrive/pdrive.serial", "r");
-    size_t serial = 100; 
-    char *linha = malloc(serial);
-    int estado;
-    if (!g)
-      {
-        perror("/etc/pam.d/pam.pdrive/pdrive.serial");
-        exit(1);
-      }
-      while (getline(&linha, &serial, g) > 0)
-      {
-            if(strcmp(buf, linha) == 0){
-                estado = 0;
+	printf("\n\n  -> Usuário root, liberado acesso sem pendrive.");
+	printf("\n          -> Logando..\n\n");
+	sleep(2);
+	return PAM_SUCCESS;
+      
+    }else{
+	  FILE *g= fopen("/etc/pam.d/pam.pdrive/pdrive.serial", "r");
+	size_t serial = 100; 
+	char *linha = malloc(serial);
+	int estado;
+	if (!g)
+	  {
+	    perror("/etc/pam.d/pam.pdrive/pdrive.serial");
+	    exit(1);
+	  }
+	  while (getline(&linha, &serial, g) > 0)
+	  {
+		if(strcmp(buf, linha) == 0){
+		    estado = 0;
 
-            }else{
-                estado = 1;
-            }
-      }
-      if (linha){
-        free(linha);
-      }
-      fclose(g);
+		}else{
+		    estado = 1;
+		}
+	  }
+	  if (linha){
+	    free(linha);
+	  }
+	  fclose(g);
 
-      if(estado == 0){
-        system("clear");
-        
-                FILE *log;
+	  if(estado == 0){
+	    system("clear");
+	    
+		    FILE *log;
 
-                log = fopen("/etc/pam.d/pam.pdrive/log/login", "a");
-                fprintf(log, "%s", "-> Serial: ");
-                fprintf(log, "%s | ", buf);
-                fprintf(log, "%d/", data_hora_atual->tm_mday);
-                fprintf(log, "%d/", data_hora_atual->tm_mon+1);
-                fprintf(log, "%d ", data_hora_atual->tm_year+1900);
-                fprintf(log, "%d:", data_hora_atual->tm_hour);
-                fprintf(log, "%d:", data_hora_atual->tm_min);
-                fprintf(log, "%d\n ", data_hora_atual->tm_sec);
+		    log = fopen("/etc/pam.d/pam.pdrive/log/login", "a");
+		    fprintf(log, "%s", "-> Serial: ");
+		    fprintf(log, "%s | ", buf);
+		    fprintf(log, "%d/", data_hora_atual->tm_mday);
+		    fprintf(log, "%d/", data_hora_atual->tm_mon+1);
+		    fprintf(log, "%d ", data_hora_atual->tm_year+1900);
+		    fprintf(log, "%d:", data_hora_atual->tm_hour);
+		    fprintf(log, "%d:", data_hora_atual->tm_min);
+		    fprintf(log, "%d\n ", data_hora_atual->tm_sec);
 
-          printf("\n\n  -> Pendrive Liberado!");
-          printf("\n          -> Logando..\n\n");
-          sleep(2);
-          return PAM_SUCCESS;
-      }else{
-        system("clear");
-          printf("\n\n  -> Pendrive bloqueado!");
-          printf("\n          -> Insira um pendrive liberado e tente novamente.\n\n");
-          sleep(2);
-          return PAM_AUTH_ERR;
-      }
+	      printf("\n\n  -> Pendrive Liberado!");
+	      printf("\n          -> Logando..\n\n");
+	      sleep(2);
+	      return PAM_SUCCESS;
+	  }else{
+	    system("clear");
+	      printf("\n\n  -> Pendrive bloqueado!");
+	      printf("\n          -> Insira um pendrive liberado e tente novamente.\n\n");
+	      sleep(2);
+	      return PAM_AUTH_ERR;
+	  }
+    }
 }
     
